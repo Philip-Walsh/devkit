5d97fd6f42307842972d691ed332bbcd3864af8a
Philip Walsh <philip.walsh@example.com>
2025-04-25 18:38:03 +0100

    chore(release): bump minor version to chore: bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="devkit",
-    version="1.0.7",
+    version="1.3.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
